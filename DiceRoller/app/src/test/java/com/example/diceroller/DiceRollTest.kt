b614257baa8dc4c782fc35223b084b83b9c37235
KaiaWalters <kaia.cwalters@gmail.com>
2021-04-01 21:57:00 -0400

    Add roboelectric to Dice Roller project

--- DiceRoller/app/src/test/java/com/example/diceroller/DiceRollTest.kt
+++ DiceRoller/app/src/test/java/com/example/diceroller/DiceRollTest.kt
@@ -1,17 +1,14 @@
 package com.example.diceroller
-
-import android.R
 import org.junit.Assert.*
 import org.junit.Test
-import android.view.View
-import org.junit.Rule
-import javax.security.auth.Subject
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-
+//@RunWith(RobolectricTestRunner.class)
 class DiceRollTest {
-
     @Test
-    fun display_random_value_when_button_is_clicked() {
+   fun displayvaluewhenbuttonisclicked()
+    {
         //arrange
         var subject: MainActivity = MainActivity()
         var diceValue = subject.resultText.text
@@ -26,10 +23,5 @@ class DiceRollTest {
 
         //assert
         assertEquals(false, actual)
-    }
-
-    @Test
-    fun display_diceValue_plus_one_when_button_is_clicked() {
-
     }
 }
